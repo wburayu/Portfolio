e08ce7cb6ddad47bdf56bc3079306a14e2a5f121
Welela Burayu <92478988+wburayu@users.noreply.github.com>
2023-12-31 02:52:17 -0600

    Add files via upload

--- assests/js/script.js
+++ assests/js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Portfolio | Saurav Mukherjee";
+        document.title = "Portfolio | Welela Burayu";
         $("#favicon").attr("href","assests/images/favicon.png");
     }
     else {
